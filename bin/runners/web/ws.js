6e03b958394b332f738893546a9f36d15a03632b
Kirill Dudchenko <dudchenko.kirill.1995@gmail.com>
2021-07-30 20:57:24 +0300

    socket.io lesson

--- bin/runners/web/ws.js
+++ bin/runners/web/ws.js
@@ -1,25 +1,43 @@
-const WebSocket = require('ws')
+//const WebSocket = require('ws')
+const sio = require('socket.io')
 
 const run = (server) => {
-    const wsServer = new WebSocket.Server({ server });
+    // const wsServer = new WebSocket.Server({ server });
+    const io = sio(server)
 
-    wsServer.on('connection', (socket) => { 
-    
-    console.log(new Date() + "new WS connection")
+    // wsServer.on('connection', (socket) => { 
+    io.on('connection', (socket) => {
+        console.log(`Connection ID: ${socket.id}`);
 
-    let sum = 0;
+        socket.on('/chat', (data) => {
+            console.log('Data: ', data);
+        })
 
-    socket.on('message', (data) => {
+        socket.on('/pow', (data, cb) => {
+            const result = Math.pow(data.val, data.lvl);
+            console.log('Pow result: ', result);
+
+            cb({result});
+        })
+
+    // let sum = 0;
+
+    // socket.on('message', (data) => {
         
-        sum+= Number(data);
-        console.log(`Frontend sent ${data}`)
-        console.log(`Sum ${sum}`)
+    //     sum+= Number(data);
+    //     console.log(`Frontend sent ${data}`)
+    //     console.log(`Sum ${sum}`)
 
-        if (sum > 10) {
-            socket.send(sum);
-        }
-    });
-});
+    //     if (sum > 10) {
+    //         socket.send(sum);
+    //     }
+    // });
+
+        socket.on('disconnect', () => {
+        console.log(`Disconnection ID: ${socket.id}`)
+
+    })
+  })
 }
 
 module.exports = run;
